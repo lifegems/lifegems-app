0f1a9cbf95e17af376ec655918373730bd18abc8
Michalik, Joshua <smileyfacepancakes@gmail.com>
2017-04-13 10:33:04 -0400

    Update config defaults

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,9 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
-      splashScreen.hide();
+      setTimeout(() => {
+        splashScreen.hide();
+      }, 100);      
       this.importMasterData();
       console.log("loading complete");
     });
