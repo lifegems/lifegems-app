cd217b8b9049758da5480f58b19ba644b40f81a5
Michalik, Joshua <smileyfacepancakes@gmail.com>
2017-04-18 15:40:17 -0400

    Enhance gems dashboard

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import * as _ from 'underscore';
+
+import { TagsService } from '../tags.service';
+import { InsightService } from '../insight.service';
+import { Tag, ArticlePage } from '../gems';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  public tags: Tag[];
+  public articlesCount: number = 0;
+  public randomArticle: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController, 
+    private tagsService: TagsService,
+    private insightService: InsightService) {
+  }
 
+  ngOnInit() {
+    this.tagsService.getTags().subscribe(tags => {
+      this.tags = _.map(tags, tag => {
+        let aTag = tag.split(".");
+        return new Tag(aTag[0], aTag[1]);
+      })
+    });
+    this.getArticlesCount();
+    this.loadRandomArticle();
+  }
+
+  getTaggedArticlesCount() {
+    return _.uniq(_.pluck(this.tags, 'article')).length || null;
+  }
+
+  getTagCount() {
+    return _.uniq(_.pluck(this.tags, 'name')).length || null;
+  }
+
+  getArticlesCount() {
+    this.insightService.getSections().subscribe(sections => {
+      _.each(sections, section => {
+        this.articlesCount += _.values(section.articles).length;
+      })
+    });
+  }
+
+  loadRandomArticle() {
+    this.insightService.getSections().subscribe(sections => {
+      let maxSectionIndex: number = _.values(sections).length - 1;
+      let randomSection: any = sections[_.random(maxSectionIndex)];
+      let articles: any[] = _.values(randomSection.articles);
+      let maxArticleIndex: number = articles.length - 1;
+      this.randomArticle = articles[maxArticleIndex];
+    });
+  }
+
+  viewArticle(article) {
+    this.navCtrl.push(ArticlePage, {
+      article: article
+    })
   }
 
 }
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,1 +1,5 @@
+export { InsightService } from './insight.service';
 export { PagesModule } from './pages.module';
+export { ReadProgressService } from './read-progress.service';
+export { TagsService } from './tags.service';
+export * from './gems';
